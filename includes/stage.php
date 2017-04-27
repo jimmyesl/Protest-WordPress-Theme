5faf8125ae48878f1fb0f6f4d4876d7c958419ec
Jimmy Soller <james.soller@gmail.com>
2017-04-27 15:04:26 -0400

    Update stage.php

--- includes/stage.php
+++ includes/stage.php
@@ -1,1 +1,13 @@
 
+<?php
+/**
+ * Stage frontpage slider 
+ *
+ *
+ * @package mybootstrap
+ */
+?>
+
+<section id="stage">
+    <?php if( function_exists('cyclone_slider') ) cyclone_slider('frontpage-slider'); ?>
+</section>
